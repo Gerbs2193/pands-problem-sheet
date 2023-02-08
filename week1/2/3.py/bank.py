26a8813ee13e2cac005c502b495872fc24a568ab
unknown <gerball2193@gmail.com>
2023-02-08 18:51:24 +0000

    pands-problem-sheet

---
+++ week1/2/3.py/bank.py
@@ -0,0 +1,9 @@
+# Bank assignment that adds two amounts and prints them in human readable form
+#bank.py
+#Author:Gerard Ball
+
+amount1 = int(input("Enter amount 1 in cents:"))
+amount2 = int(input("Enter amount 2 in cents:"))
+sum= int(65)+int(180)
+
+print (f" Sum of these is €{2.45}")
---
+++ week1/2/3.py/hello.py
@@ -0,0 +1,4 @@
+print("hello Ger")
+#Program just prints Hello Ger
+#hello.py
+#Author=Gerard Ball
