a88d45ddc13d5ea968a97fa97e5a64729d09f7d0
unknown <gerball2193@gmail.com>
2023-02-20 14:45:14 +0000

    week04Collatz.py

--- week03accounts.py
+++ week03accounts.py
@@ -2,7 +2,7 @@
 #week3accounts.py
 #masking numbers
 
-amount = int(input("Enter Bank Account Number"))
+amount = int(input("Enter Bank Account Number ending (6789)"))
 str = "123456789"
 strlength = len(str)
 masked = strlength - 4
