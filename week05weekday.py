48cc8b413e780138212a330f2210ec3900eed7a7
unknown <gerball2193@gmail.com>
2023-02-22 20:48:23 +0000

    week05weekday.py

--- week05weekday.py
+++ week05weekday.py
@@ -1,6 +1,13 @@
+# Code that tells you if the day is a weekday :( or a weekend :)
+# Author: Gerard Ball
+# Week05weekday.py
+
+
 import datetime
 weekno = datetime.datetime.today().weekday()
+#Monday, Tuesday, Wednesday, Thursday, Friday
 if weekno < 5:
     print ("Yes, today is a Weekday - how positively wonderful...")
+# weekno>5=Saturday, Sunday
 else: 
     print ("Weekend, yessssss!")
