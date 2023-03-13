bca47d5cc9f8368a0de11c322b9f84474bf0c5eb
unknown <gerball2193@gmail.com>
2023-03-13 21:18:30 +0000

    week07letterfrequency.py

--- ggg.py
+++
@@ -1,5 +0,0 @@
-def letterfrequency(filename, letter):
-      file = open(filename, 'r')
-      text = file.read()
-      return text.count(letter)
-print(letterfrequency('week07testing.txt', 'e'))
--- testtypes.py
+++
@@ -1,4 +0,0 @@
-numberToRound = float(input("Enter a float number:"))
-roundedNumber = round(numberToRound)
-print ( '{} rounded is {}'.format(numberToRound,roundedNumber))
-
