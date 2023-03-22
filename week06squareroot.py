a32c8bd2d9d1241a307ba7a8309bf28953b67581
unknown <gerball2193@gmail.com>
2023-03-22 19:34:01 +0000

    week06squareroot.py

--- week06squareroot.py
+++ week06squareroot.py
@@ -2,12 +2,31 @@
 #program that creates a function that uses Newton's formula to approximate the square rot of a number
 #week06squareroot.py
 
+# defines a function 'my_sqrt' that takes argument of 'n'
 def my_sqrt(n):
+
+    #variable approx = half of 'n'. Newton approach to approximating a number is the square root of a number is always less than or equal to half said number. Starting point approx variable
     approx = n/2
+
+    # calcuates new approximation of n. Average of approx and n/approx is now closer to to actual square root. This approximation is .ocated in the variable 'closer'
     closer = (approx +n/approx)/2
+
+    # while loop. Continues for as long as approximation closer is different from previous result
     while closer != approx:
+
+        # inside while loop, the below lines update the vaules of approx and closer for as many iterations of loop until satisfactory result is returned. Initially, i did this only once and committed this to github. However, simply adding 3 successive iterations garners a very accurate square root. ex. 81 = 9.01 whereas before it was 45
+         
         approx = closer
         closer = (approx + n/approx)/2
+        approx = closer
+        closer = (approx + n/approx)/2
+        approx = closer
+        closer = (approx + n/approx)/2
+        approx = closer
+        closer = (approx + n/approx)/2
+
+        #exits loop and returns final square root calculation/approximation
         return approx
+        
 n=int(input("enter number: "))
 print ("Square Root of Number: ", my_sqrt(n))
