201d353c57206d2a9188bbf970bf8e7f48b4a04d
unknown <gerball2193@gmail.com>
2023-04-11 18:59:50 +0100

    bank2.py

--- bank2.py
+++ bank2.py
@@ -2,13 +2,15 @@
 #bank.py
 #Author:Gerard Ball
 
-# variable name amount 1 then int(input) to convert string input to integer
 amount1 = int(input("first amount in cents: "))
-
 amount2 = int(input("Second amount in cents: "))
-
-#variable answer is both inputs sum/100
-answer = (amount1+amount2)/100
-
-#print the answer
-print(f"The sum: €{answer}")
+# variable sum_in_cents is both inputs sum
+sum_in_cents = amount1 + amount2
+# divide sum_in_cents by 100 to get sum in euros as an integer
+sum_in_euros = sum_in_cents // 100
+# get the remaining cents by taking the modulo of sum_in_cents and 100
+cents = sum_in_cents % 100
+# format the output string to display the sum in euros and cents
+output_string = "The sum: €{}.{:02d}".format(sum_in_euros, cents)
+# print the output string
+print(output_string)
