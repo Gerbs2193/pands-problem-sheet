8ac87140033ce2ecaa2774bb00ba234039443761
unknown <gerball2193@gmail.com>
2023-04-11 17:58:53 +0100

    weekday5.py

--- weekday5.py
+++ weekday5.py
@@ -5,7 +5,7 @@
 # imported datemine module to get access to datetime
 import datetime
 
-#
+#Variable weekno and the rest used to get current time which then calls the weekday method to get the day of the week (0-6)
 weekno = datetime.datetime.today().weekday()
 
 #Monday, Tuesday, Wednesday, Thursday, Friday
@@ -13,6 +13,8 @@ if weekno < 5:
 
     print ("Yes, today is a Weekday - how positively wonderful...")
 # weekno>5=Saturday, Sunday
+
+#6,7 aka sat + sun 
 
 else: 
  print ("Weekend, yessssss!")
